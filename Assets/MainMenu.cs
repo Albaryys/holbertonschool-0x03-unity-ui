8467ca2da5fc4071d36c06694bed2aaf38e678a4
Albaryys <a.tily.pro@gmail.com>
2024-01-28 16:57:41 +0100

    add task 12

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,8 +5,20 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
     public void PlayMaze()
     {
+        trapMat.color = Color.red;
+        goalMat.color = Color.green;
+
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
